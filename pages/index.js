5a96ec2a3f642221e896999f61512e19b4485294
BelugaWhaleSam <sameermohd2k1@gmail.com>
2023-04-17 18:17:38 +0530

    updated-headline

--- pages/index.js
+++ pages/index.js
@@ -40,14 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+          <h1> Fast Feedback</h1>
         </div>
 
         <div className={styles.grid}>
