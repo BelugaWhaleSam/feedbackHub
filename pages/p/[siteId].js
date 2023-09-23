e69b5d5901392c52d20470fa62ab9094ab813341
BelugaWhaleSam <sameermohd2k1@gmail.com>
2023-09-23 15:56:22 +0530

    static-regen

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -19,6 +19,7 @@ export async function getStaticProps(context) {
         props: {
             initalFeedback: feedback,
         }, // will be passed to the page component as props
+        unstable_revalidate: 1,
     };
 }
 
